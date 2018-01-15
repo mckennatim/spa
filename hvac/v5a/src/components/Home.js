7e65dec5dc2e42bb0c69a34127b14862d579cc5d
mckennatim <mckenna.tim@gmail.com>
2018-01-15 13:26:26 -0500

    11-hvac-v5

---
+++ hvac/v5a/src/components/Home.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import {router} from '../app'
+import {pStyle} from '../styles'
+const style = {
+	...pStyle, outer: {...pStyle.outer, background: '#C4A265'}
+}
+pStyle.outer.background='#C4A265'
+
+const Home = (props) =>{
+	function goAbout(){
+		console.log("in home goAbout")
+		router.navigate('/about');
+	}
+		const style = {
+		...pStyle, outer: {...pStyle.outer, background: '#CC66CC'}
+	}
+	return(
+		<div style={style.outer}>
+			<h3> Home </h3>
+			<button id="but" onClick={goAbout}>goto about</button>
+		</div>
+	)
+}
+
+export {Home}
