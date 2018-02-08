04d4471178704651358c7905689e462fbd33257d
mckennatim <mckenna.tim@gmail.com>
2018-02-08 12:58:55 -0500

    30-hvac-v6p-mqttFor-componentWillUpdate

--- README.md
+++ README.md
@@ -7,6 +7,8 @@ https://blog.risingstack.com/introducing-react-easy-state/?utm_campaign=React%2B
 
 https://cloudinary.com/console/welcome
 ## tags
+### 30-hvac-v6p-mqttFor-componentWillUpdate
+TODO when moving back from 12ParleyVale/peri we need to reconstitute zdat.
 ### 29-hvac-v6o-SenRel
 If you click on a zone in zones it adds an `sr` parameter to locs params. After Loc succesfully loads zone and device data, it checks to see if there is an `sr`. If so it only sends Zones that one zone. If Zones sees only one zone it diplays SenRel. Meanwhile data keeps flowing from mqtt.
 param
---
+++ hvac/v6p-mqttFor-componentWillUpdate/src/routing.js
@@ -0,0 +1,27 @@
+import Navigo from 'navigo';
+import { switchPage} from './actions/responsive';
+
+var router
+
+const routing = ()=>{
+  const cfg ={root: null, useHash: true}
+  router = new Navigo(cfg.root, cfg.useHash);
+  router
+    .on({
+      'loclist': ()=> {switchPage({name: 'LocList', params: null});} ,
+      'login': ()=> {switchPage({name: 'Login', params: null});} ,
+      'registered': (params, query)=> {switchPage({name: 'Registered', params: {...params, query: query}});} ,
+      'products': ()=> {switchPage({name: 'Products', params: null});} ,
+      'products/:id': (params)=>{switchPage({name: 'Products', params: params});},
+      'at/:loc': (params)=>{switchPage({name: 'Loc', params: params});},
+      'at/:loc/:sr': (params)=>{switchPage({name: 'Loc', params: params});},
+      'about': ()=>{switchPage({name: 'About', params: null});},
+      'dog': ()=>{switchPage({name: 'Dog', params: null});},
+      'home': ()=>{switchPage({name: 'Home', params: null});},
+      '*': ()=>{switchPage({name: 'Home', params: null});}
+    })
+    .resolve();
+  return router
+}
+
+export {routing}
